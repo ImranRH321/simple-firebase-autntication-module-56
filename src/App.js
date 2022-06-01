1c1fbc46f6995aad2caf98b9fc18af169ba02643
ImranRH321 <imranrh247@gmail.com>
2022-06-01 07:18:00 +0600

    part-4

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import app from "./firebase.init";
 
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
 const handleGoogleAutntication = () => {
-  console.log('click');
-}
+  signInWithPopup(auth, provider)
+    .then(result => {
+      const user = result.user;
+      console.log(user);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
 
 function App() {
-
   return (
     <div>
       <h1>firebase authentication</h1>
